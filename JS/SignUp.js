86d5e9a9b4a78f0827a97c6a1a861ba0ad5d52e4
justin-swenson111 <justinswenson111@gmail.com>
2024-11-18 18:20:27 -0700

    need to fix id check

--- JS/SignUp.js
+++ JS/SignUp.js
@@ -6,24 +6,85 @@ const db =getDatabase()
 
 var empCheck = document.getElementById("emp")
 var stuCheck = document.getElementById("stu")
-var empEmail = document.getElementById("empEmail")
+var Email = document.getElementById("empEmail")
 var idNUm = document.getElementById("idNum")
 var Pass = document.getElementById("Pass")
+
+var AccType=""
+var passlength =""
+var idlength =""
+var IdDoesntexist
 
 var create = document.getElementById("create")
 
 create.addEventListener('click', createUser)
+
 // create.onclick=function(){createUser()}
 
 function createUser(){
-    set(ref(db, "Students/"+ idNUm.value),{
-      ID: idNUm.value,
-      Password: Pass.value
-  })
-    .then(()=>{
-      alert("Data added successfully");
-  })
-  .catch((error)=>{
-      alert(error);
-  });
+  passlength=Pass.value
+  idlength=idNUm.value
+  alert(idlength.length)
+  alert(passlength.length)
+      if((stuCheck.checked || empCheck.checked) && Email.value!=""){
+        if(idlength.length==5){
+          if(passlength.length>=8){
+            IDexists()
+          }
+          else{
+            alert("Password must be at least 8 characters")
+
+          }
+          }
+        else{
+          alert("ID must be 5 characters")
+        }
+      }
+      else{
+        alert("please fill out all fields")
+      }
+  }
+  function IDexists(){
+    const dbref = ref(db);
+
+    let IDcheck =get(child(dbref,"Requests/"+ idNUm.value))
+    .then((snapshot)=>{
+      snapshot.val().ID
+    })
+    alert(string(IDcheck)+"hi")
+
+  }
+  function addUser(){
+    if(stuCheck.checked){
+      AccType="Student"
+      set(ref(db, "Requests/"+ idNUm.value),{
+        ID: idNUm.value,
+        Password: Pass.value,
+        Type: AccType,
+        Email: Email.value
+    })
+      .then(()=>{
+        alert("Data added successfully");
+    })
+    .catch((error)=>{
+        alert(error);
+    });
+    }
+
+    else{
+
+      AccType="Employer"
+      set(ref(db, "Requests/"+ idNUm.value),{
+        ID: idNUm.value,
+        Password: Pass.value,
+        Type: AccType,
+        Email: Email.value
+    })
+      .then(()=>{
+        alert("Data added successfully");
+    })
+    .catch((error)=>{
+        alert(error);
+    });
+    }
   }
